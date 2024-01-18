f3938af10689f6b1e69e709777253c63ab2b44d8
Chen Yu <chenyu.nus@gmail.com>
2024-01-18 03:17:46 +0000

    local change

--- jam/berries/plot_utils.py
+++ jam/berries/plot_utils.py
@@ -1,33 +1,102 @@
+from typing import Optional, Tuple, List, Callable, Union
 from jax.typing import ArrayLike
-import bokeh as bk
-from typing import NamedTuple
-from bokeh.plotting import figure
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from matplotlib.image import AxesImage
+from matplotlib.lines import Line2D
+
+AxesSubplot = Axes
+
+mpl.rcParams['savefig.transparent'] = True
+mpl.rcParams['axes.spines.top'] = False
+mpl.rcParams['axes.spines.right'] = False
+
+# mpl.rcParams["font.sans-serif"] = ["Fira Sans Regular", "Candara",
+#                                    "Optima", "Arial"]
+mpl.rcParams["font.sans-serif"] = ["Fira Sans"]
+mpl.rcParams["font.family"] = "sans-serif"
+mpl.rcParams["font.weight"] = "regular"
+mpl.rcParams['axes.titleweight'] = "regular"
+# mpl.rcParams['figure.titleweight'] = "medium"
+mpl.rcParams["axes.labelweight"] = "regular"
+mpl.rcParams['mathtext.fontset'] = 'custom'
+mpl.rcParams['mathtext.rm'] = 'Fira Sans'
+mpl.rcParams['mathtext.sf'] = 'Fira Sans'
+mpl.rcParams['mathtext.cal'] = 'Fira Sans'
+mpl.rcParams['mathtext.it'] = 'Fira Sans:normalitalic'
+mpl.rcParams['mathtext.bf'] = 'Fira Sans:bold'
+mpl.rcParams['mathtext.tt'] = 'Fira Code:medium'
+
+plt.rc('grid', linestyle="--", color='black', alpha=0.1)
 
 
-class PlotSize(NamedTuple):
-    width: int
-    height: int
+
+def visualize_matrix(mat: ArrayLike,
+                     dpi: int = 300) -> Tuple[Figure, AxesSubplot, AxesImage]:
+    f, _ax = plt.subplots(figsize=(5, 5), dpi=dpi)
+    _ax.spines['left'].set_visible(False)
+    _ax.spines['bottom'].set_visible(False)
+    _ax.spines['right'].set_visible(False)
+    _ax.spines['top'].set_visible(False)
+    pt = _ax.matshow(mat, cmap="Greys")
+    cax = f.colorbar(pt, shrink=0.8, drawedges=False)
+    cax.outline.set_visible(False)
+    return f, _ax, pt
 
 
-def plot_xys(
-    x: ArrayLike,
-    ys: list[ArrayLike],
-    title: str = "",
-    size: PlotSize = PlotSize(900, 300),
-    labels: list[str] = None,
-):
-    # plot multiple series using bokeh
-    plot = figure(title=title, width=size.width, height=size.height)
-    colors = bk.palettes.Category20[20]
-    for i, y in enumerate(ys):
-        color = colors[i % len(colors)]
-        if labels is None:
-            label = f"color {i}"
-        elif len(labels) != len(ys):
-            raise ValueError(f"labels must be same length as ys")
+def bounded_f_line(f: Callable[[float], float],
+                   inv_f: Callable[[float], float],
+                   _ax: AxesSubplot, **kwargs) -> Optional[Line2D]:
+    e = 0.000001
+    x_min, x_max = _ax.get_xbound()
+    y_min, y_max = _ax.get_ybound()
+    fx_min = f(x_min)
+    fx_max = f(x_max)
+    fy_min = inv_f(y_min)
+    fy_max = inv_f(y_max)
+    x_range = [x_min, x_max]
+    y_range = [fx_min, fx_max]
+    in_range = True
+    if not (y_min - e <= fx_min <= y_max + e):
+        if x_max + e >= fy_max >= fy_min >= x_min - e:
+            x_range[0], y_range[0] = fy_min, y_min
+        elif x_max + e >= fy_min >= fy_max >= x_min - e:
+            x_range[0], y_range[0] = fy_max, y_max
         else:
-            label = labels[i]
-        plot.line(x, y, legend_label=label, line_width=2, color=color)
-    plot.legend.location = "top_left"
+            in_range = False
+    if not (y_min - e <= fx_max <= y_max + e):
+        if x_max + e >= fy_max >= fy_min >= x_min - e:
+            x_range[1], y_range[1] = fy_max, y_max
+        elif x_max + e >= fy_min >= fy_max >= x_min - e:
+            x_range[1], y_range[1] = fy_min, y_min
+        else:
+            in_range = False
 
-    return plot
+    if in_range:
+        line = Line2D(x_range, y_range, **kwargs)
+        _ax.add_line(line)
+        return line
+    else:
+        return None
+
+
+def bounded_line(p1: ArrayLike, p2: ArrayLike,
+                 _ax: AxesSubplot,
+                 **kwargs) -> Optional[Line2D]:
+    if p2[0] == p1[0]:
+        return _ax.axvline(x=p1[0])
+    elif p2[1] == p1[1]:
+        return _ax.axhline(y=p1[1])
+    else:
+        k = (p2[1] - p1[1]) / (p2[0] - p1[0])
+
+        def f(x):
+            return p1[1] + k * (x - p1[0])
+
+        def inv_f(y):
+            return p1[0] + 1 / k * (y - p1[1])
+
+        return bounded_f_line(f, inv_f, _ax, **kwargs)
